ee6fc769f01a8ac3b9dd1296809c0c0451520997
David Lains <dlains@gmail.com>
2021-05-13 18:00:51 -0400

    Closes #4 Updated to latest angular version.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'tome-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'tome';
 }
