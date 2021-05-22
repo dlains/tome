d540f730ee7ad7f2ad026d52746ce306b3a743f4
David Lains <dlains@gmail.com>
2021-05-21 21:50:46 -0400

    Closes #10 Post and PostView components added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
+import { PostViewComponent } from './post-view/post-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PostComponent,
+    PostViewComponent
   ],
   imports: [
     BrowserModule,
