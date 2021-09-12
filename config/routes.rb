a2a1bde40a27500cad8bdb7acf7f7b708751512d
David Lains <dlains@gmail.com>
2021-09-12 13:33:25 -0400

    Closes #47 Logout button added.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post 'sign_up', to: 'registrations#create'
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create', as: 'log_in'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
 
   resources :posts
   resources :tags, only: %i[index show]
