d2adf061fbd71ba982e58096d11aaa74cd2ff434
David Lains <dlains@gmail.com>
2021-09-18 10:02:07 -0400

    Closes #57 Capistrano deploy scripts in place.

---
+++ config/deploy.rb
@@ -0,0 +1,78 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+set :keep_releases,           3
+set :application,             "tome"
+set :repo_url,                "git@github.com:dlains/tome.git"
+set :migration_role,          :app
+set :puma_threads,            [4, 16]
+set :puma_workers,            0
+
+set :puma_bind,               "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,              "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,                "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log,         "#{shared_path}/log/puma.error.log"
+set :puma_error_log,          "#{shared_path}/log/puma.access.log"
+set :puma_preload_app,        true
+set :puma_worker_timeout,     nil
+set :puma_init_active_record, true
+
+# Ask which branch to deploy. This will come in handy if I ever need to deploy
+# from a branch other than master.
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+desc "Check that we can access everything"
+task :check_write_permissions do
+  on roles(:all) do |host|
+    if test("[ -w #{fetch(:deploy_to)} ]")
+      info "#{fetch(:deploy_to)} is writable on #{host}"
+    else
+      error "#{fetch(:deploy_to)} is not writable on #{host}"
+    end
+  end
+end
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+end
